3064882fb5bde8ab3913263c73b52dbf524b0d2d
OrNixz <ramadhanafif172@gmail.com>
2023-10-04 21:42:47 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -7,7 +7,7 @@ import streamlit as st
 sns.set(style='dark')
 
 # Menyiapkan data day_df
-day_df = pd.read_csv("data/day.csv")
+day_df = pd.read_csv(r"day.csv")
 day_df.head()
 
 # Menghapus kolom yang tidak diperlukan
@@ -171,8 +171,12 @@ ax.plot(
     monthly_rent_df['count'],
     marker='o', 
     linewidth=2,
-    color="#3498eb"
+    color='tab:blue'
 )
+
+for index, row in enumerate(monthly_rent_df['count']):
+    ax.text(index, row + 1, str(row), ha='center', va='bottom', fontsize=12)
+
 ax.tick_params(axis='x', labelsize=25, rotation=45)
 ax.tick_params(axis='y', labelsize=20)
 st.pyplot(fig)
@@ -200,6 +204,10 @@ sns.barplot(
     ax=ax
 )
 
+for index, row in season_rent_df.iterrows():
+    ax.text(index, row['registered'], str(row['registered']), ha='center', va='bottom', fontsize=12)
+    ax.text(index, row['casual'], str(row['casual']), ha='center', va='bottom', fontsize=12)
+
 ax.set_xlabel(None)
 ax.set_ylabel(None)
 ax.tick_params(axis='x', labelsize=20, rotation=0)
@@ -212,11 +220,18 @@ st.subheader('Weatherly Rentals')
 
 fig, ax = plt.subplots(figsize=(16, 8))
 
+colors=["tab:blue", "tab:orange", "tab:green"]
+
 sns.barplot(
     x=weather_rent_df.index,
     y=weather_rent_df['count'],
+    palette=colors,
     ax=ax
 )
+
+for index, row in enumerate(weather_rent_df['count']):
+    ax.text(index, row + 1, str(row), ha='center', va='bottom', fontsize=12)
+
 ax.set_xlabel(None)
 ax.set_ylabel(None)
 ax.tick_params(axis='x', labelsize=20)
@@ -228,12 +243,21 @@ st.subheader('Weekday, Workingday, and Holiday Rentals')
 
 fig, axes = plt.subplots(nrows=3, ncols=1, figsize=(15,10))
 
+colors1=["tab:blue", "tab:orange"]
+colors2=["tab:blue", "tab:orange"]
+colors3=["tab:blue", "tab:orange", "tab:green", "tab:red", "tab:purple", "tab:brown", "tab:pink"]
+
 # Berdasarkan workingday
 sns.barplot(
     x='workingday',
     y='count',
     data=workingday_rent_df,
+    palette=colors1,
     ax=axes[0])
+
+for index, row in enumerate(workingday_rent_df['count']):
+    axes[0].text(index, row + 1, str(row), ha='center', va='bottom', fontsize=12)
+
 axes[0].set_title('Number of Rents based on Working Day')
 axes[0].set_ylabel(None)
 axes[0].tick_params(axis='x', labelsize=15)
@@ -244,7 +268,12 @@ sns.barplot(
   x='holiday',
   y='count',
   data=holiday_rent_df,
+  palette=colors2,
   ax=axes[1])
+
+for index, row in enumerate(holiday_rent_df['count']):
+    axes[1].text(index, row + 1, str(row), ha='center', va='bottom', fontsize=12)
+
 axes[1].set_title('Number of Rents based on Holiday')
 axes[1].set_ylabel(None)
 axes[1].tick_params(axis='x', labelsize=15)
@@ -255,7 +284,12 @@ sns.barplot(
   x='weekday',
   y='count',
   data=weekday_rent_df,
+  palette=colors3,
   ax=axes[2])
+
+for index, row in enumerate(weekday_rent_df['count']):
+    axes[2].text(index, row + 1, str(row), ha='center', va='bottom', fontsize=12)
+
 axes[2].set_title('Number of Rents based on Weekday')
 axes[2].set_ylabel(None)
 axes[2].tick_params(axis='x', labelsize=15)
